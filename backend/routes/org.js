3ec8f764883b9c4e618ed1a7a205c6e05c1c4c02
ShivamP48 <89608318+ShivamP48@users.noreply.github.com>
2022-09-28 23:23:26 -0500

    Add files via upload
    org route

---
+++ backend/routes/org.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+
+//importing data model schemas
+let { org } = require("../models/models"); 
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    org.find( 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+//GET single entry by ID
+router.get("/id/:id", (req, res, next) => { 
+    org.find({ _id: req.params.id }, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+});
+
+//POST
+router.post("/", (req, res, next) => { 
+    org.create( 
+        req.body, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//PUT
+router.put("/:id", (req, res, next) => {
+    org.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+
+
+module.exports = router;
